377c9b3963616f6187fd2d183f8e35355abb2d7a
rohan-new <rrohanrocks10@gmail.com>
2018-09-26 08:26:45 +0530

    new

--- server/server.js
+++ server/server.js
@@ -16,11 +16,16 @@ var io = socketIo(server);
 // Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+if(process.env.NODE_ENV === 'production'){
+
+
 app.use(express.static(path.join(__dirname, "client/build")))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+}
 
 
 
